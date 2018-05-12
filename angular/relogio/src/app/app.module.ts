c6c5915c82a237436f05b0781d2c99675cd8afc1
Alfonso <alfonsojunior@gmail.com>
2018-05-12 11:38:18 -0300

    Angular Inicial e Relógio

---
+++ angular/relogio/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HorarioComponent } from './horario/horario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HorarioComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/relogio/src/app/horario/horario.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horario',
+  templateUrl: './horario.component.html',
+  styleUrls: ['./horario.component.css']
+})
+export class HorarioComponent implements OnInit {
+
+  fullDate = new Date();
+  hora = this.fullDate.toLocaleDateString() + ' ' + this.fullDate.toLocaleTimeString();
+
+  constructor() { }
+
+  ngOnInit() {
+    setInterval( () => {
+      this.fullDate = new Date();
+      this.hora = this.fullDate.toLocaleDateString() + ' ' + this.fullDate.toLocaleTimeString();
+    }, 1000);
+  }
+
+}
